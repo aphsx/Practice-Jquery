32b525f27312bcaf7f25855d5b26967754c97109
aphsx <168500857+aphsx@users.noreply.github.com>
2024-05-08 14:02:53 +0700

    Jquery

---
+++ index.js
@@ -0,0 +1,9 @@
+// $("button").click(function(){
+//     $("h1").css("color","yellow");
+// });
+// $("input").keypress(function(event){
+//     $("h1").html(event.key)
+// })
+$("h1").on("mouseover",function(){
+    $("h1").css("color","purple");
+})
